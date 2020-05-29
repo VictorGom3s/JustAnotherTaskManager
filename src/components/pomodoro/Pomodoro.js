713f63315a4995da5b4e73c47276acf289c7553e
Victor Sampaio <vsampaio@pm.me>
2020-05-28 22:16:34 -0300

    Notification at the timer's end

--- src/components/pomodoro/Pomodoro.js
+++ src/components/pomodoro/Pomodoro.js
@@ -7,12 +7,13 @@ import swal from "sweetalert";
 
 const Pomodoro = () => {
   const [intervalID, setIntervalID] = useState(0);
-  let [minutes, setMinutes] = useState(25);
-  let [seconds, setSeconds] = useState(0);
+  let [minutes, setMinutes] = useState(0);
+  let [seconds, setSeconds] = useState(5);
   const [running, setRunning] = useState(false);
 
   function startTimer() {
     setRunning(true);
+    getNotificationPermission();
   }
 
   function pauseTimer() {
@@ -27,11 +28,29 @@ const Pomodoro = () => {
     setRunning(false);
   }
 
+  const getNotificationPermission = async () => {
+    if (!("Notification" in window)) {
+      alert("This browser does not support desktop notification.");
+      return false;
+    }
+
+    if (Notification.permission === "default") {
+      Notification.requestPermission();
+    }
+  };
+
+  async function notify(msg) {
+    if (Notification.permission === "granted") {
+      new Notification("Just Another Task Manager", { body: msg });
+    }
+  }
+
   useEffect(() => {
     if (running) {
       const id = setInterval(timer, 1000);
       setIntervalID(id);
     } else if (minutes === 0 && seconds === 0) {
+      notify("Time's ended");
       swal("Good job", "Time to take a walk!", "warning");
       resetTimer();
     }
